5b76642bd173cb52e92f3316185ae789c18eddbc
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-05-23 20:44:12 -0300

    Primeiros passos com JS

---
+++ Listas/Lista05/ex01/js/ex01.js
@@ -0,0 +1,6 @@
+var nome = prompt('Digite o seu nome')
+var idade = Number(prompt('Digite sua idade'))
+var emprego = prompt('Digite emprego atual')
+var endereco = prompt('Digite o seu endereco')
+
+alert(`Seu nome é ${nome}\nVoce tem ${idade} anos\nSeu emprego atual e: ${emprego}\nE mora no endereco: ${endereco}`)
---
+++ Listas/Lista05/ex02/ex02.js
@@ -0,0 +1,6 @@
+var nome = 'Erineu'
+var idade = 57
+var emprego = 'Astronauta'
+var endereco = 'Rua dos Ratones'
+
+console.log(`Seu nome é ${nome}\nVoce tem ${idade} anos\nSeu emprego atual e: ${emprego}\nE mora no endereco: ${endereco}`)
