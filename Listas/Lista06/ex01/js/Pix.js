cefec9e3c790f157fd2c83ef91ba16cf70a3bf86
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-05-31 11:52:48 -0300

    Adicionando Lista 6

---
+++ Listas/Lista06/ex01/js/Pix.js
@@ -0,0 +1,31 @@
+var pix = {
+    chavePix: "",
+    valorTransferencia: 0.0,
+    dataTransacao: "",
+    descricaoPagamento: "",
+    pagador: {
+        nome: "",
+        documento: ""
+    },
+    recebedor: {
+        nome: "",
+        documento: ""
+    },
+    historicoTransacoes: [
+        {
+            data: "",
+            valor: 0.0,
+            descricao: ""
+        },
+        {
+            data: "",
+            valor: 0.0,
+            descricao: ""
+        },
+        {
+            data: "",
+            valor: 0.0,
+            descricao: ""
+        }
+    ]
+}
