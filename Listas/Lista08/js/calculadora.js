643040597f179494e2405b91393bce09e9610f49
DanMartExe <daniel.martins1@aluno.ifsp.edu.br>
2024-06-20 23:00:45 -0300

    Atualizando p/ calculos em sequencia

--- Listas/Lista08/js/calculadora.js
+++ Listas/Lista08/js/calculadora.js
@@ -1,6 +1,6 @@
 let digitos = document.getElementById('digitos')
 let tela = document.getElementById('tela')
-let numeroDigitado = ''
+let valorAtualizado = ''
 
 for (let indiceDigito = 0; indiceDigito <= 9; indiceDigito++) {
     let botaoDigito = document.createElement('button')
@@ -9,8 +9,8 @@ for (let indiceDigito = 0; indiceDigito <= 9; indiceDigito++) {
     botaoDigito.id = `btn-${indiceDigito}`
 
     botaoDigito.addEventListener('click', (evento) => {
-        numeroDigitado += evento.target.value
-        tela.textContent = numeroDigitado
+        valorAtualizado += evento.target.value
+        tela.textContent = valorAtualizado
     })
 
     digitos.appendChild(botaoDigito)
@@ -33,33 +33,33 @@ criarBotaoOperador('btn-div', '/')
 criarBotaoOperador('btn-result', '=')
 
 document.getElementById('btn-ponto').addEventListener('click', (evento) => {
-    numeroDigitado += evento.target.value
-    tela.textContent = numeroDigitado
+    valorAtualizado += evento.target.value
+    tela.textContent = valorAtualizado
 })
 
 document.getElementById('btn-soma').addEventListener('click', (evento) => {
-    numeroDigitado += evento.target.value
-    tela.textContent = numeroDigitado
+    valorAtualizado += evento.target.value
+    tela.textContent = valorAtualizado
 })
 
 document.getElementById('btn-sub').addEventListener('click', (evento) => {
-    numeroDigitado += evento.target.value
-    tela.textContent = numeroDigitado
+    valorAtualizado += evento.target.value
+    tela.textContent = valorAtualizado
 })
 
 document.getElementById('btn-mult').addEventListener('click', (evento) => {
-    numeroDigitado += evento.target.value
-    tela.textContent = numeroDigitado
+    valorAtualizado += evento.target.value
+    tela.textContent = valorAtualizado
 })
 
 document.getElementById('btn-div').addEventListener('click', (evento) => {
-    numeroDigitado += evento.target.value
-    tela.textContent = numeroDigitado
+    valorAtualizado += evento.target.value
+    tela.textContent = valorAtualizado
 })
 
 document.getElementById('btn-result').addEventListener('click', (evento) => {
-    tela.textContent = "Resultado: " + eval(numeroDigitado)
-    numeroDigitado = ''
+    tela.textContent = "Resultado: " + eval(valorAtualizado)
+    valorAtualizado = eval(valorAtualizado)
 })
 
 let botaoLimpar = document.createElement('button')
@@ -68,6 +68,6 @@ botaoLimpar.textContent = 'C'
 digitos.appendChild(botaoLimpar)
 
 document.getElementById('btn-limpar').addEventListener('click', (evento) => {
-    numeroDigitado = ''
+    valorAtualizado = ''
     tela.textContent = ''
 })
